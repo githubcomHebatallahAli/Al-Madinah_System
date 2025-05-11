1db9bab4399873d6d8f5f243d9d079811b5642ee
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-12 02:25:24 +0300

    update

--- app/Traits/TracksChangesTrait.php
+++ app/Traits/TracksChangesTrait.php
@@ -6,18 +6,24 @@ namespace App\Traits;
 trait TracksChangesTrait
 {
     public function getChangedData(array $oldData, array $newData): array
-    {
-        $changed = [];
+{
+    $ignoredKeys = ['updated_at', 'creationDate', 'creationDateHijri'];
+    $changed = [];
 
-        foreach ($newData as $key => $newValue) {
-            if (array_key_exists($key, $oldData) && $oldData[$key] !== $newValue) {
-                $changed[$key] = [
-                    'old' => $oldData[$key],
-                    'new' => $newValue,
-                ];
-            }
+    foreach ($newData as $key => $newValue) {
+        if (in_array($key, $ignoredKeys)) {
+            continue;
         }
 
-        return $changed;
+        if (array_key_exists($key, $oldData) && $oldData[$key] !== $newValue) {
+            $changed[$key] = [
+                'old' => $oldData[$key],
+                'new' => $newValue,
+            ];
+        }
     }
+
+    return $changed;
+}
+
 }
