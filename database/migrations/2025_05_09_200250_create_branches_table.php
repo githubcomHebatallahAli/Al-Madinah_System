e963792105a5f80923cd1f92896cfbec41821955
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-12 03:55:55 +0300

    update

--- database/migrations/2025_05_09_200250_create_branches_table.php
+++ database/migrations/2025_05_09_200250_create_branches_table.php
@@ -20,7 +20,8 @@ return new class extends Migration
             $table->unsignedBigInteger('tripsCount')->default(0);
             $table->unsignedBigInteger('storesCount')->default(0);
             $table->unsignedBigInteger('workersCount')->default(0);
-            $table->timestamp('creationDate')->nullable();
+             $table->dateTime('creationDate')->nullable();
+            $table->string('creationDateHijri')->nullable();
             $table->enum('status', ['active', 'notActive'])->default('active');
             $table->json('changed_data')->nullable();
             $table->timestamps();
