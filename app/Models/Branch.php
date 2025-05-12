338cd0b94b72f3b56fbf4be54acaa3adf83d9add
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-12 08:26:54 +0300

    update

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -58,38 +58,24 @@ class Branch extends Model
 protected static function booted()
 {
     static::saved(function ($branch) {
-        // عندما يتم إضافة أو تعديل الفرع، نقوم بتحديث عدد الفروع في المدينة المرتبطة
-        if ($branch->city) {
-            $branch->city->update([
-                'branchesCount' => $branch->city->branches()->count()
-            ]);
-        }
-    });
-
-    static::updating(function ($branch) {
-        // نخزن المدينة القديمة في الذاكرة فقط إذا كان سيتم تغيير city_id
-        if ($branch->isDirty('city_id')) {
-            $branch->setAttribute('old_city_id', $branch->getOriginal('city_id'));  // تخزين مؤقت في الذاكرة
-        }
-    });
-
-    static::updated(function ($branch) {
-        // إذا كان هناك تغيير في city_id، نقوم بتحديث المدينة القديمة والجديدة
-        if (isset($branch->old_city_id) && $branch->old_city_id != $branch->city_id) {
-            // تحديث المدينة القديمة
-            $oldCity = \App\Models\City::find($branch->old_city_id);
-            if ($oldCity) {
-                $oldCity->update([
-                    'branchesCount' => $oldCity->branches()->count()
-                ]);
+        // إذا كان هناك تغيير في المدينة
+        if ($branch->wasChanged('city_id')) {
+            // تقليل العدد في المدينة القديمة
+            if (!is_null($branch->getOriginal('city_id'))) {
+                City::where('id', $branch->getOriginal('city_id'))
+                    ->decrement('branchesCount');
             }
 
-            // تحديث المدينة الجديدة
-            if ($branch->city) {
-                $branch->city->update([
-                    'branchesCount' => $branch->city->branches()->count()
-                ]);
+            // زيادة العدد في المدينة الجديدة
+            if ($branch->city_id) {
+                City::where('id', $branch->city_id)
+                    ->increment('branchesCount');
             }
+        }
+        // إذا كان إنشاء جديد وليس تحديث
+        elseif ($branch->wasRecentlyCreated && $branch->city_id) {
+            City::where('id', $branch->city_id)
+                ->increment('branchesCount');
         }
     });
 }
