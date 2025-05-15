c42acf5af8c7d2cc32332377a00c9f61083f30a8
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-15 21:19:13 +0300

    update

--- routes/Apis/Admin/worker.php
+++ routes/Apis/Admin/worker.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\WorkerController;
 
 
-Route::controller(WorkerController::class)->prefix('/adminOrBranchManger')->middleware(['admin','worker'])->group(
+Route::controller(WorkerController::class)->prefix('/adminOrBranchManger')->middleware('adminOrWorker')->group(
     function () {
 
    Route::get('/showAll/worker','showAll');
