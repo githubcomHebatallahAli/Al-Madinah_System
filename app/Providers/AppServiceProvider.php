e36d84e1596c07c85eb6bab0e6f69a3d6bde558c
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-26 08:28:52 +0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,30 +25,14 @@ class AppServiceProvider extends ServiceProvider
             return    Auth::guard('admin')->check()&& $user->role_id == 1;
         });
 
-// Gate::define('manage_system', function ($user) {
-//     return $user && (
-//         ($user instanceof Admin && $user->role_id == 1) ||
-//         ($user instanceof WorkerLogin && $user->role_id == 2)
-//     );
-// });
+Gate::define('manage_system', function ($user) {
+    return $user && (
+        ($user instanceof Admin && $user->role_id == 1) ||
+        ($user instanceof WorkerLogin && $user->role_id == 2)
+    );
+});
 
-    Gate::define('manage_system', function ($user) {
-        if (!$user) return false;
 
-        // للـ Admin
-        if ($user instanceof \App\Models\Admin) {
-            return $user->role_id == 1 && $user->status == 'active';
-        }
-
-        // للـ WorkerLogin
-        if ($user instanceof \App\Models\WorkerLogin) {
-            return $user->role_id == 2 &&
-                   $user->status == 'active' &&
-                   $user->dashboardAccess == 'ok';
-        }
-
-        return false;
-    });
 
     }
 
