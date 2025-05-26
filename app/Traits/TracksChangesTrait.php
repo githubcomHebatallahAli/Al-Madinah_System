4480fb81d2ce204b11c1787e735cf72f3b88b31a
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-26 20:09:59 +0300

    update

--- app/Traits/TracksChangesTrait.php
+++ app/Traits/TracksChangesTrait.php
@@ -5,6 +5,55 @@ namespace App\Traits;
 trait TracksChangesTrait
 {
 
+    public function getChangedData(array $oldData, array $newData): array
+{
+    $alwaysTrack = ['creationDate', 'creationDateHijri'];
+    $ignoredKeys = [
+        'updated_at',
+        'updated_by',
+        'updated_by_type',
+        'changed_data'
+    ];
+
+    $changed = [];
+
+    foreach ($newData as $key => $newValue) {
+        if (in_array($key, $ignoredKeys)) {
+            continue;
+        }
+
+        $oldValue = $oldData[$key] ?? null;
+
+        // تتبع دائم أو اختلاف القيم
+        if (in_array($key, $alwaysTrack) || $oldValue != $newValue) {
+            $changed[$key] = [
+                'old' => $oldValue,
+                'new' => $newValue,
+            ];
+
+            // في حالة الحقول من نوع *_id نضيف الاسم أيضاً إن وجد
+            if (str_ends_with($key, '_id')) {
+                $relationName = str_replace('_id', '', $key);
+                try {
+                    $oldModel = $this->getOriginalModelFromId($relationName, $oldValue);
+                    $newModel = $this->$relationName;
+
+                    if ($oldModel || $newModel) {
+                        $changed[$key]['old_name'] = $oldModel?->name;
+                        $changed[$key]['new_name'] = $newModel?->name;
+                    }
+                } catch (\Throwable $e) {
+                    // لو العلاقة مش موجودة نتجاهل بصمت
+                }
+            }
+        }
+    }
+
+    return $changed;
+}
+
+
+
 
 // public function getChangedData(array $oldData, array $newData): array
 // {
@@ -26,78 +75,60 @@ trait TracksChangesTrait
 
 //         $oldValue = $oldData[$key] ?? null;
 
-//         // تتبع دائم للحقول المطلوبة
+//         // تتبع دائم أو تغير فعلي
 //         if (in_array($key, $alwaysTrack) || $oldValue != $newValue) {
 //             $changed[$key] = [
 //                 'old' => $oldValue,
 //                 'new' => $newValue,
 //             ];
+
+//             // لو المفتاح عبارة عن علاقة ID (مثل city_id)
+//             if (str_ends_with($key, '_id')) {
+//                 $relation = str_replace('_id', '', $key); // city
+
+//                 // لو العلاقة معرفة في الموديل
+//                 if (method_exists($this, $relation)) {
+//                     try {
+//                         $relatedModel = $this->$relation()->getRelated();
+
+//                         $oldModel = $relatedModel->find($oldValue);
+//                         $newModel = $relatedModel->find($newValue);
+
+//                         // جلب الاسم المناسب
+//                         $oldName = optional($oldModel)->name ?? optional($oldModel)->title ?? null;
+//                         $newName = optional($newModel)->name ?? optional($newModel)->title ?? null;
+
+//                         // فقط لو الاسم تغير
+//                         if ($oldName != $newName) {
+//                             $changed[$relation . '_name'] = [
+//                                 'old' => $oldName,
+//                                 'new' => $newName,
+//                             ];
+//                         }
+//                     } catch (\Throwable $e) {
+//                         // تجاهل الخطأ إذا العلاقة غير موجودة أو غير معرفة
+//                     }
+//                 }
+//             }
 //         }
 //     }
 
 //     return $changed;
 // }
 
-public function getChangedData(array $oldData, array $newData): array
+
+protected function getOriginalModelFromId(string $relation, $id)
 {
-    $alwaysTrack = ['creationDate', 'creationDateHijri'];
+    $relationMethod = $this->$relation();
 
-    $ignoredKeys = [
-        'updated_at',
-        'updated_by',
-        'updated_by_type',
-        'changed_data'
-    ];
-
-    $changed = [];
-
-    foreach ($newData as $key => $newValue) {
-        if (in_array($key, $ignoredKeys)) {
-            continue;
-        }
-
-        $oldValue = $oldData[$key] ?? null;
-
-        // تتبع دائم أو تغير فعلي
-        if (in_array($key, $alwaysTrack) || $oldValue != $newValue) {
-            $changed[$key] = [
-                'old' => $oldValue,
-                'new' => $newValue,
-            ];
-
-            // لو المفتاح عبارة عن علاقة ID (مثل city_id)
-            if (str_ends_with($key, '_id')) {
-                $relation = str_replace('_id', '', $key); // city
-
-                // لو العلاقة معرفة في الموديل
-                if (method_exists($this, $relation)) {
-                    try {
-                        $relatedModel = $this->$relation()->getRelated();
-
-                        $oldModel = $relatedModel->find($oldValue);
-                        $newModel = $relatedModel->find($newValue);
-
-                        // جلب الاسم المناسب
-                        $oldName = optional($oldModel)->name ?? optional($oldModel)->title ?? null;
-                        $newName = optional($newModel)->name ?? optional($newModel)->title ?? null;
-
-                        // فقط لو الاسم تغير
-                        if ($oldName != $newName) {
-                            $changed[$relation . '_name'] = [
-                                'old' => $oldName,
-                                'new' => $newName,
-                            ];
-                        }
-                    } catch (\Throwable $e) {
-                        // تجاهل الخطأ إذا العلاقة غير موجودة أو غير معرفة
-                    }
-                }
-            }
-        }
+    if (method_exists($relationMethod, 'getRelated')) {
+        $relatedModel = $relationMethod->getRelated();
+        return $relatedModel->find($id);
     }
 
-    return $changed;
+    return null;
 }
+
 
 
 
