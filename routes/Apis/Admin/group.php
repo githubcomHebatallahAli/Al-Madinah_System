83037b79a997aa0f5b2479ca3788149695c601b4
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-05-28 01:23:44 +0300

    update

---
+++ routes/Apis/Admin/group.php
@@ -0,0 +1,18 @@
+
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\GroupController;
+
+
+Route::controller(GroupController::class)->prefix('/adminOrBranchManger')->middleware('adminOrWorker')->group(
+    function () {
+
+   Route::get('/showAll/group','showAll');
+   Route::post('/create/group', 'create');
+   Route::get('/edit/group/{id}','edit');
+   Route::post('/update/group/{id}', 'update');
+   Route::patch('notActive/group/{id}', 'notActive');
+    Route::patch('active/group/{id}', 'active');
+   });
--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,5 @@ require __DIR__ . '/Apis/Admin/store.php';
 require __DIR__ . '/Apis/Admin/trip.php';
 require __DIR__ . '/Apis/Admin/office.php';
 require __DIR__ . '/Apis/Admin/campaign.php';
+require __DIR__ . '/Apis/Admin/group.php';
 
