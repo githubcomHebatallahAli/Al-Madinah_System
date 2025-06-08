72b46da2e53c5db09943eae9da3b35a415f38eb0
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-09 00:48:03 +0300

    update

--- routes/Apis/Admin/store.php
+++ routes/Apis/Admin/store.php
@@ -12,13 +12,13 @@ Route::post('/create/store', 'create');
 Route::post('/update/store/{id}', 'update');
 Route::patch('notActive/store/{id}', 'notActive');
 Route::patch('active/store/{id}', 'active');
-Route::get('/showAll/title/withoutPaginate','showAllWithoutPaginate');
+Route::get('/showAll/store/withoutPaginate','showAllWithoutPaginate');
    });
 Route::controller(StoreController::class)->prefix('/adminOrBranchManger')->middleware('adminOrWorker')->group(
     function () {
 
    Route::get('/showAll/store','showAll');
    Route::get('/edit/store/{id}','edit');
-     Route::get('/showAll/title/withPaginate','showAllWithPaginate');
+     Route::get('/showAll/store/withPaginate','showAllWithPaginate');
 
    });
