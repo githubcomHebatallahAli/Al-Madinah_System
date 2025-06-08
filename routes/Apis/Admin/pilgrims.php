54545b3aab3ec8b2d63f7e964bcd1240392ae341
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-08 14:48:05 +0300

    update

--- routes/Apis/Admin/pilgrims.php
+++ routes/Apis/Admin/pilgrims.php
@@ -6,7 +6,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PilgrimsController;
 
 
-Route::controller(PilgrimsController::class)->prefix('/adminOrBranchMangerOrDelegate')->middleware('adminOrWorker')->group(
+// Route::controller(PilgrimsController::class)->prefix('/adminOrBranchMangerOrDelegate')->middleware('adminOrWorker')->group(
+//     function () {
+
+Route::controller(PilgrimsController::class)->prefix('/delegate')->middleware('adminOrWorker')->group(
     function () {
 
    Route::get('/showAll/pilgrims','showAll');
