8c3c13a5c841cf8358d8f91359ac6bcd1082bf86
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-10 08:44:41 +0300

    update

--- routes/Apis/Admin/shipmentInvoice.php
+++ routes/Apis/Admin/shipmentInvoice.php
@@ -10,7 +10,8 @@ use App\Http\Controllers\Admin\ShipmentInvoiceController;
 Route::controller(ShipmentInvoiceController::class)->prefix('/supplier')->middleware('adminOrWorker')->group(
     function () {
 
-   Route::get('/showAll/shipmentInvoice','showAll');
+   Route::get('/showAll/shipmentInvoice','showAllWithoutPaginate');
+   Route::get('/showAll/shipmentInvoice/withPaginate','showAllWithPaginate');
    Route::post('/create/shipmentInvoice', 'create');
     Route::put('/updatePaid/shipmentInvoice/{id}','updatePaidAmount');
    Route::get('/edit/shipmentInvoice/{id}','edit');
