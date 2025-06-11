75c0fce7f80c1f8e282eee7ede8475af7b1846a9
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-11 09:33:59 +0300

    update

---
+++ routes/Apis/Admin/busTrip.php
@@ -0,0 +1,21 @@
+
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\BusTripController;
+
+
+Route::controller(BusTripController::class)->prefix('/delegate')->middleware('adminOrWorker')->group(
+    function () {
+
+   Route::get('/showAll/busTrip','showAllWithoutPaginate');
+   Route::get('/showAll/busTrip','showAllWihtPaginate');
+   Route::post('/create/busTrip', 'create');
+   Route::get('/edit/busTrip/{id}','edit');
+   Route::post('/update/busTrip/{id}', 'update');
+    Route::patch('notActive/busTrip/{id}', 'notActive');
+    Route::patch('active/busTrip/{id}', 'active');
+
+   });
