9ff82d0e8c0d9341a061455d344cd71c773d85d3
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-13 22:08:05 +0300

    update

--- app/Http/Requests/Admin/BusInvoiceRequest.php
+++ app/Http/Requests/Admin/BusInvoiceRequest.php
@@ -24,7 +24,7 @@ class BusInvoiceRequest extends FormRequest
         'group_id'=>'required|exists:groups,id',
         'worker_id'=>'nullable|exists:workers,id',
         'payment_method_type_id'=>'required|exists:payment_method_types,id',
-        // 'seatPrice' => 'required|numeric|min:0',
+
         'discount'=>'nullable|numeric|min:0|max:99999.99',
         'tax'=>'nullable|numeric|min:0|max:99999.99',
         'paidAmount'=>'nullable|numeric|min:0|max:99999.99',
@@ -33,11 +33,20 @@ class BusInvoiceRequest extends FormRequest
         'paymentStatus'=>'nullable|in:pending,paid,refunded',
         'creationDate' =>'nullable|date_format:Y-m-d H:i:s',
         'creationDateHijri'=>'nullable|string',
-        'pilgrims' => 'nullable|array',
-        'pilgrims.*.id' => 'required|exists:pilgrims,id',
+        // 'pilgrims' => 'nullable|array',
+        // 'pilgrims.*.id' => 'required|exists:pilgrims,id',
+        // 'pilgrims.*.seatNumber' => 'required|array|min:1', // ✅ السماح بأكثر من مقعد
+        // 'pilgrims.*.seatNumber.*' => 'required|string',
+        // 'pilgrims.*.type' => 'nullable|string',
+        // 'pilgrims.*.position' => 'nullable|string',
 
-        // 'pilgrims.*.seatNumber' => 'required|string',
-        'pilgrims.*.seatNumber' => 'required|array|min:1', // ✅ السماح بأكثر من مقعد
+        'pilgrims' => 'nullable|array',
+        'pilgrims.*.idNum' => 'nullable|string|exists:pilgrims,idNum', // 🔹 جعل "idNum" قابلة للتحقق
+        'pilgrims.*.name' => 'required_if:pilgrims.*.idNum,null|string|max:255', // 🔹 مطلوب فقط إذا لم يكن المعتمر مسجلًا مسبقًا
+        'pilgrims.*.phoNum' => 'nullable|string|max:20',
+        'pilgrims.*.nationality' => 'required_if:pilgrims.*.idNum,null|string|max:50',
+        'pilgrims.*.gender' => 'required_if:pilgrims.*.idNum,null|in:male,female,child',
+        'pilgrims.*.seatNumber' => 'required|array|min:1',
         'pilgrims.*.seatNumber.*' => 'required|string',
         'pilgrims.*.type' => 'nullable|string',
         'pilgrims.*.position' => 'nullable|string',
