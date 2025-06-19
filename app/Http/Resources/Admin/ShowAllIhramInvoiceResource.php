20f5dbead8e13fb6e5048961ff8482fa5d059ab3
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-19 23:24:37 +0300

    update

--- app/Http/Resources/Admin/ShowAllIhramInvoiceResource.php
+++ app/Http/Resources/Admin/ShowAllIhramInvoiceResource.php
@@ -7,13 +7,28 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ShowAllIhramInvoiceResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+
+                 return [
+            "id" => $this->id,
+            "ihramSuppliesCount"=> $this->ihramSuppliesCount,
+  
+
+
+            'bus_invoice_id'=> $this ->busInvoice?->id,
+            'payment_method_type_id' => $this->paymentMethodType?->id,
+            'payment_method_type' => $this->paymentMethodType?->type,
+            'payment_method_type_by' => $this->paymentMethodType?->by,
+
+            'total' => $this->total,
+            'paidAmount' => $this->paidAmount,
+
+            'invoiceStatus' => $this->invoiceStatus,
+            'paymentStatus' => $this->paymentStatus,
+            'creationDateHijri' => $this->creationDateHijri,
+            'creationDate' => $this->creationDate,
+        ];
     }
 }
