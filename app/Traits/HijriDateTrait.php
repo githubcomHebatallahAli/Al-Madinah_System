462102f002cc8e55c1290ee30933968c415c7836
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-22 20:11:14 +0300

    update

--- app/Traits/HijriDateTrait.php
+++ app/Traits/HijriDateTrait.php
@@ -7,36 +7,36 @@ use Illuminate\Support\Facades\Http;
 
 trait HijriDateTrait
 {
-      public function getHijriDate()
-    {
-        $now = now()->timezone('Asia/Riyadh');
-
-        $response = Http::get('https://api.aladhan.com/v1/gToH', [
-            'date' => $now->format('d-m-Y'),
-        ]);
-
-        $hijri = $response['data']['hijri'];
-
-        return "{$hijri['weekday']['ar']} {$hijri['day']} {$hijri['month']['ar']} {$hijri['year']} - {$now->format('H:i:s')}";
-    }
-
-    //     public function getHijriDate($date = null)
+    //   public function getHijriDate()
     // {
-    //     $date = $date
-    //         ? Carbon::parse($date)->timezone('Asia/Riyadh')
-    //         : now()->timezone('Asia/Riyadh');
+    //     $now = now()->timezone('Asia/Riyadh');
 
     //     $response = Http::get('https://api.aladhan.com/v1/gToH', [
-    //         'date' => $date->format('d-m-Y'),
+    //         'date' => $now->format('d-m-Y'),
     //     ]);
 
-    //     if ($response->ok() && isset($response['data']['hijri'])) {
-    //         $hijri = $response['data']['hijri'];
+    //     $hijri = $response['data']['hijri'];
 
-    //         return "{$hijri['weekday']['ar']} {$hijri['day']} {$hijri['month']['ar']} {$hijri['year']} - {$date->format('H:i:s')}";
-    //     }
-
-    //     return null;
+    //     return "{$hijri['weekday']['ar']} {$hijri['day']} {$hijri['month']['ar']} {$hijri['year']} - {$now->format('H:i:s')}";
     // }
+
+        public function getHijriDate($date = null)
+    {
+        $date = $date
+            ? Carbon::parse($date)->timezone('Asia/Riyadh')
+            : now()->timezone('Asia/Riyadh');
+
+        $response = Http::get('https://api.aladhan.com/v1/gToH', [
+            'date' => $date->format('d-m-Y'),
+        ]);
+
+        if ($response->ok() && isset($response['data']['hijri'])) {
+            $hijri = $response['data']['hijri'];
+
+            return "{$hijri['weekday']['ar']} {$hijri['day']} {$hijri['month']['ar']} {$hijri['year']} - {$date->format('H:i:s')}";
+        }
+
+        return null;
+    }
 
 }
