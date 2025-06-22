d93c7965f62f04480edc6f5f2af92cd5efface2b
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-06-22 20:43:08 +0300

    update

--- app/Traits/HijriDateTrait.php
+++ app/Traits/HijriDateTrait.php
@@ -22,22 +22,32 @@ trait HijriDateTrait
     // }
 
 
-public function getHijriDate(string $gregorianDateTime)
+public function getHijriDate(?string $gregorianInput = null)
 {
-    $date = \Carbon\Carbon::parse($gregorianDateTime)->timezone('Asia/Riyadh');
+    try {
+        $date = $gregorianInput
+            ? \Carbon\Carbon::parse($gregorianInput)->timezone('Asia/Riyadh')
+            : now()->timezone('Asia/Riyadh');
 
-    $response = Http::get('https://api.aladhan.com/v1/gToH', [
-        'date' => $date->format('d-m-Y'),
-    ]);
+        $response = Http::get('https://api.aladhan.com/v1/gToH', [
+            'date' => $date->format('d-m-Y'),
+        ]);
 
-    if (!$response->ok()) {
-        return response()->json(['message' => 'فشل في جلب التاريخ الهجري'], 500);
+        if (!$response->ok()) {
+            return response()->json(['message' => 'فشل في جلب التاريخ الهجري من API'], 500);
+        }
+
+        $hijri = $response['data']['hijri'];
+
+        return "{$hijri['weekday']['ar']} {$hijri['day']} {$hijri['month']['ar']} {$hijri['year']} - {$date->format('H:i:s')}";
+    } catch (\Exception $e) {
+        Log::error('getHijriDate error: ' . $e->getMessage());
+        return response()->json([
+            'message' => 'حدث خطأ أثناء تحويل التاريخ: ' . mb_convert_encoding($e->getMessage(), 'UTF-8', 'UTF-8')
+        ], 500);
     }
-
-    $hijri = $response['data']['hijri'];
-
-    return "{$hijri['weekday']['ar']} {$hijri['day']} {$hijri['month']['ar']} {$hijri['year']} - {$date->format('H:i:s')}";
 }
+
 
 
 
